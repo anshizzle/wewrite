17612f3b69c3ce908ee9190235df31e80588b54e
Anshul Jain <mosquito242@gmail.com>
2014-10-25 10:52:46 -0700

    installed bootstrap, annotate; ran annotate

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: lines
+#
+#  id            :integer          not null, primary key
+#  text          :string(255)
+#  score         :integer
+#  depth         :integer
+#  previous_line :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class Line < ActiveRecord::Base
 end
--- test/models/line_test.rb
+++ test/models/line_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: lines
+#
+#  id            :integer          not null, primary key
+#  text          :string(255)
+#  score         :integer
+#  depth         :integer
+#  previous_line :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 require 'test_helper'
 
 class LineTest < ActiveSupport::TestCase
